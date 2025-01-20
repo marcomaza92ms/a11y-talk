e165def9f58537e1ffa0b28c8b4d1e5f6a5c173a
marcomaza92ms <mmaza@makingsense.com>
2025-01-20 09:47:11 -0300

    feat: add focus trap y modal

--- src/NoA11y/NoA11y.jsx
+++ src/NoA11y/NoA11y.jsx
@@ -1,15 +1,69 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 import "./NoA11y.css";
 
+const Modal = ({ isOpen, onClose }) => {
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter" || event.key === "Escape") {
+      onClose();
+    }
+  };
+
+  if (!isOpen) return null;
+
+  return ReactDOM.createPortal(
+    <div className="modal">
+      <div className="modal-content">
+        <div
+          tabIndex="0"
+          className="close"
+          onKeyDown={handleKeyDown}
+          onClick={onClose}
+        >
+          &times;
+        </div>
+        <div tabIndex="0">Random Text 1</div>
+        <div tabIndex="0">Random Text 2</div>
+        <div tabIndex="0">Random Text 3</div>
+      </div>
+    </div>,
+    document.getElementById("root")
+  );
+};
+
+Modal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
+
 const NoA11y = () => {
+  const [isModalOpen, setModalOpen] = React.useState(false);
+
+  const openModal = () => setModalOpen(true);
+  const closeModal = () => setModalOpen(false);
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      openModal();
+    }
+  };
+
   return (
     <>
-      <div>this is a header</div>
-      <div>
+      <div className="header">this is a header</div>
+      <div className="main">
         this is the main content
         <div>this is an article</div>
         <div>this is an aside section</div>
+        <button onClick={openModal}>Open Modal</button>
       </div>
-      <div>this is a footer</div>
+      <div className="footer">this is a footer</div>
+      <Modal
+        onKeyDown={handleKeyDown}
+        isOpen={isModalOpen}
+        onClose={closeModal}
+      />
     </>
   );
 };
